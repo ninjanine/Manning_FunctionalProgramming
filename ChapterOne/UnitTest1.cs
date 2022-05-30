3d203950bf616e6eba2e365804e2523327628c8b
BETTERBOND\David.Francis <david.francis@betterlife.co.za>
2022-05-30 14:30:17 +0200

    Update to C# 10

--- ChapterOne/UnitTest1.cs
+++ ChapterOne/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace ChapterOne
@@ -8,7 +9,13 @@ namespace ChapterOne
         [Fact]
         public void Tripples()
         {
-            
+            var triple = (int x) => x * 3;
+ 
+            var range = Enumerable.Range(1, 3);
+ 
+            var triples = range.Select(triple);
+ 
+            //triples // => [3, 6, 9]
         }
     }
 }

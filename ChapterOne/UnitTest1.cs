b586c909c50ba6fe9ec90857c8aff87693ab7e30
BETTERBOND\David.Francis <david.francis@betterlife.co.za>
2022-05-30 12:37:10 +0200

    Initial commit

---
+++ ChapterOne/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ChapterOne
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Tripples()
+        {
+            
+        }
+    }
+}
